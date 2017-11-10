7d4d9517ff3534d19f6fb8267cb6001effdd9ca4
Carlos Velasco <carlos@2activedesign.com>
2017-11-10 10:47:34 +0100

    add jquery

--- entry.js
+++ entry.js
@@ -1,5 +1,9 @@
+window.jQuery = require("jquery");
+window.$ = require("jquery");
+require("hammerjs");
 require("./node_modules/materialize-css/dist/css/materialize.css");
 require("./node_modules/materialize-css/dist/js/materialize");
+// require('./css/materialdesignicons.css');
 require("./css/master.css");
 require("./js/offlineRuntimeInstall");
 require("./js/master");
